88b9ab396fc11e8543a2c1b9e0637c215ebc0cc9
aduhan8000 <aduhan8000@gmail.com>
2026-03-14 13:42:02 -0400

    Add files via upload

--- act_1.sql
+++ act_1.sql
@@ -1,14 +1,39 @@
-CREATE TABLE supplier(
-    SNO TEXT PRIMARY KEY,
-    SNAME TEXT,
-    STATUS INTEGER,
-    CITY TEXT
+CREATE TABLE IF NOT EXISTS Salesman(
+    Salesman_id TEXT PRIMARY KEY,
+    name TEXT,
+    city TEXT,
+    Commision REAL
 );
-INSERT INTO  supplier (SNO,SNAME, STATUS, CITY) VALUES
-('S1','Smith',20,'London'),
-('S2','Jones',10,'Paris'),
-('S3','Blake',30,'Paris'),
-('S4','Clarke', 20, 'London'),
-('S5','Adams',30,'Athens');
 
-SELECT  * FROM supplier;
+INSERT INTO Salesman (Salesman_id,name,city,Commision) VALUES 
+('5001','James Hong','New York',0.15)
+('5002','James Smith','New Jersey',0.3)
+('5005','Pit Alex','London',0.11)
+('5006', 'Mc Lyon', 'Paris',0.14)
+('5007','Paul Adamn','Rome',0.13)
+('5003','Lausen Hen','San Jose',0.12);
+SELECT * FROM Salesman;
+CREATE TABLE IF NOT EXISTS Orders (
+    ord_no TEXT PRIMARY KEY ,
+    purch_amt REAL,
+    ord_date TEXT,
+    customer_id TEXT,
+    Salesman_id TEXT
+);
+INSERT INTO Orders (ord_no,purch_amt,ord_date,customer_id,Salesman_id)VALUES
+('70001',150.5, '2022-10-5','3005','5002'),
+('70009',270.5, '2022-10-5','3001','5001'),
+('70002',65.26,'2012-10-05','3002','5003'),
+('70004',110.5,'2012-08-17','3007','5005'),
+('70007',948.5,'2012-09-10','3005','5005'),
+('70005',2400.6,'2012-07-27','3007','5006');
+SELECT * FROM Orders;
+
+SELECT name,Comission
+FROM Salesman;
+
+
+
+
+
+
